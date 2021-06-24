0079abcba409eb714745c726642ca8497e9e59c7
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-06-24 17:56:10 -0600

    Addiding files to the repo

---
+++ info_collect.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+    Welcome <?php echo $_GET["name"]; ?>
+    <br>
+    Your email address: <?php echo $_GET["email"]; ?> has been sent to us for contacting you. Thank you!
+</body>
+</html>
